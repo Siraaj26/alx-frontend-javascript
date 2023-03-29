1d2f7fc94fc147b74c3cb708754933d5689f29ab
Siraaj Jeftha <63967985+Siraaj26@users.noreply.github.com>
2023-03-29 15:13:41 +0200

    updated main

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,74 +1,69 @@
-interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    location: string;
-    yearsOfExperience?: number;
-    [key: string]: any
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
 }
 
-interface Directors extends Teacher {
-    numberOfReports: number;
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
 }
 
-const teacher1: Teacher = {
-    firstName: 'Frank',
-    lastName: 'Donald',
-    fullTimeEmployee: true,
-    location: 'New Jersey',
-    contract: false,
-}
-
-console.log(teacher1);
-
-const director1: Directors = {
-    firstName: 'John',
-    lastName: 'Smith',
-    location: 'America',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-  };
-
-console.log(director1);
-
-interface printTeacherFunction {
-    (firstName: string, lastName: string): string;
-}
-
-export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName[0]}. ${lastName}`;
-
-console.log(printTeacher('john', 'doe'));
-
-interface StudentClassInterface {
-    firstName: string;
-    lastName: string;
-}
-
-interface StudentClassConstructorInterface {
-    new(firstName: string, lastName: string): StudentClassInterface; 
-}
-
-class StudentClass implements StudentClassInterface {
-    firstName: string;
-    lastName: string;
-
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return 'Working from home';
     }
 
-    workOnHomework(): string {
-        return 'Currently working';
+    getCoffeeBreak(): string {
+        return 'Getting a coffee break';
     }
 
-    displayName(): string {
-        return this.firstName;
+    workDirectorTasks(): string {
+        return 'Getting to director tasks';
     }
 }
 
-function createStudent(cStudent: StudentClassConstructorInterface, firstName: string, lastName: string): StudentClassInterface {
-    return new cStudent(firstName, lastName);
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return 'Cannot work from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Cannot have a break';
+    }
+
+    workTeacherTasks(): string {
+        return 'Getting to work';
+    }
 }
 
-const student1 = createStudent(StudentClass, 'mike', 'tyson');
-console.log(student1);
+function createEmployee(salary: number | string): Teacher | Director {
+    return typeof salary === 'number' && salary < 500 ? new Teacher() : new Director();
+}
+
+console.log(createEmployee(200));
+
+console.log(createEmployee(1000));
+
+console.log(createEmployee('$500'));
+
+const isDirector = (employee: Teacher | Director): boolean => employee instanceof Director;
+
+const executeWork = (employee: Teacher | Director): string => {
+    let res;
+    isDirector(employee) ? res = (employee as Director).workDirectorTasks() : res = (employee as Teacher).workTeacherTasks();
+    return res;
+};
+
+console.log(executeWork(createEmployee(200)));
+
+console.log(executeWork(createEmployee(1000)));
+
+type Subjects = 'Math' | 'History';
+
+const teachClass = (todayClass: Subjects): string => todayClass === 'Math' ? 'Teaching Math' : 'Teaching History';
+
+console.log(teachClass('Math'));
+
+console.log(teachClass('History'));
